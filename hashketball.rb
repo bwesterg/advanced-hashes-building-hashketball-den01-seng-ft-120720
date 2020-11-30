81c9b8839e766b115cee9251fd2dba5ca5ce491d
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-30 22:15:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,10 @@
 def game_hash
   game_hash = {
     :home => {
-      :team_name => "",
-      :colors => [""],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [{
-         :"Alan Anderson" => {
+          :player_name => "Alan Anderson",
           :number => "0",
           :shoe => "16",
           :points => "22",
@@ -16,7 +16,7 @@ def game_hash
           :slam_dunks => "1"
        
       },
-         :"Reggie Evans" => {
+          :player_name => "Reggie Evans",
           :number => "30",
           :shoe => "14",
           :points => "12",
@@ -27,7 +27,7 @@ def game_hash
           :slam_dunks => "7"
        
       },
-         :"Brook Lopez" => {
+          :player_name => "Brook Lopez",
           :number => "11",
           :shoe => "17",
           :points => "17",
@@ -38,7 +38,7 @@ def game_hash
           :slam_dunks => "15"
        
       },
-         :"Mason Plumlee" => {
+          :player_name => "Mason Plumlee",
           :number => "1",
           :shoe => "19",
           :points => "26",
@@ -49,7 +49,7 @@ def game_hash
           :slam_dunks => "5"
        
       },
-         :"Jason Terry" => {
+          :player_name => "Jason Terry",
           :number => "31",
           :shoe => "15",
           :points => "19",
