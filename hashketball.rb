1d28fdc4ffcd2d0c530f2e7710fbd30e78fcec21
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-30 21:59:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,6 +5,39 @@ def game_hash
       :team_name => "",
       :colors => [""],
       :players => [{
+         :Alan_Anderson => {
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => "12", 
+          :assists => "12", 
+          :steals => "3", 
+          :blocks => "1", 
+          :slam_dunks => "1"
+       
+      },
+         :Reggie_Evans => {
+          :number => "30",
+          :shoe => "14",
+          :points => "12",
+          :rebounds => "12", 
+          :assists => "12", 
+          :steals => "12", 
+          :blocks => "12", 
+          :slam_dunks => "7"
+       
+      },
+         :player_name => {
+          :number => "value",
+          :shoe => "value",
+          :points => "value",
+          :rebounds => "value", 
+          :assists => "value", 
+          :steals => "value", 
+          :blocks => "value", 
+          :slam_dunks => "value"
+       
+      },
          :player_name => {
           :number => "value",
           :shoe => "value",
