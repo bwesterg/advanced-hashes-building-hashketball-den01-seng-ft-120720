12fb4b0332ee6daad37a8f32e3244ca082c4770b
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-30 21:21:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,13 @@
 # Write your code here!
 def game_hash
   game_hash = {
-    :home => {},
+    :home => {
+      :team_name = "",
+      :colors = [""],
+      :players = [{
+        
+      }]
+    },
     :away => {}
   }
 
