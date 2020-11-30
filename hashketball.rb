93b289fbe1eaa873230fad4eda2c1ad682f62e19
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-30 21:52:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,17 @@ def game_hash
       :team_name => "",
       :colors => [""],
       :players => [{
-         :player_name => {value,
-         :number => value,
-         :shoe => value,
-         :points => value,
-         :rebounds => value, 
-         :assists => value, 
-         :steals => value, 
-         :blocks => value, 
-         :slam_dunks => value
+         :player_name => {
+         :number => "value",
+         :shoe => "value",
+         :points => "value",
+         :rebounds => "value", 
+         :assists => "value", 
+         :steals => "value", 
+         :blocks => "value", 
+         :slam_dunks => "value"
+       
+      }
        
       }]
     },
